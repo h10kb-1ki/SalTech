4d3d66114dcbb79acfe42d21ccd66330592d330d
HM <hmillionsalt@gmail.com>
2023-06-11 16:34:30 +0900

    page insert

---
+++ pages/.ipynb_checkpoints/OCR-checkpoint.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from io import BytesIO
+import base64
+from google.oauth2 import service_account
+from google.cloud import vision
+
+img = Image.open('logo.png')
+st.image(img, use_column_width=True)
+
+st.title('光学文字認識')
+st.write('Powered by google Cloud Vision')
+file_type = st.selectbox('画像のファイル形式を選択', ['png', 'jpg', 'jpeg' 'gif', 'bmp', 'tif', 'svg'])
+uploaded_file = st.file_uploader("ファイルアップロード", type=file_type, accept_multiple_files=False)
+
+# Create API client #streamlit: app dashboard > Secretsでsecretsを編集
+# https://docs.streamlit.io/knowledge-base/tutorials/databases/gcs
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
+if uploaded_file:
+    image=Image.open(uploaded_file)
+    img_array = np.array(image)
+    st.image(img_array,caption = 'プレビュー',use_column_width = True)
+
+    client = vision.ImageAnnotatorClient(credentials=credentials)
+
+    #PILで開いた画像をbase64形式に変換
+    def pil_image_to_base64(image):
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        str_encode_file = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        return str_encode_file
+
+    content = pil_image_to_base64(image)
+    ocr_image = vision.Image(content=content)
+    response =  client.document_text_detection(
+            image=ocr_image,
+            image_context={'language_hints': ['ja']},
+            #image_context={'language_hints': ['en']}
+        )
+    output_text = ''
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    output_text += ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                output_text += '\n'
+    st.write('■OCR結果：')
+    st.write(output_text)
---
+++ pages/data_analysis.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from PIL import Image
+
+st.title('Data Analysis')
+image = Image.open('論文の読み方.png')
+st.image(image)
+
+a = st.number_input('介入群・Outcome(+)', step=1)
+b = st.number_input('介入群・Outcome(-)', step=1)
+c = st.number_input('対象群・Outcome(+)', step=1)
+d = st.number_input('対象群・Outcome(-)', step=1)
+
+btn = st.button('計算')
+if btn:
+    EER = a/(a+b)
+    CER = c/(c+d)
+    RR = EER/CER
+    RRR = 1-RR
+    ARR = CER-EER
+    NNT = 1/ARR
+    st.write('')
+    st.write(f'介入群合計: {a+b} 例  \n' + 
+             f'対象群合計: {c+d} 例  \n' +
+             f'Outcome(+)合計: {a+c} 例  \n' +
+             f'Outcome(-)合計: {b+d} 例  \n' +
+             f'総計: {a+b+c+d}例'
+             )
+    st.write('---')
+    st.write(f'介入群の発生率 (EER): {EER*100:.1f} %  \n' +
+             f'対象群の発生率 (CER): {CER*100:.1f} %  \n' +
+             f'相対リスク (RR): {RR*100:.1f} %  \n' +
+             f'相対リスク減少率 (RRR): {RRR*100:.1f} %  \n' +
+             f'絶対リスク減少率 (ARR): {ARR*100:.1f} %  \n' +
+             f'治療必要数 (NNT): {NNT:.0f}  \n'
+             )

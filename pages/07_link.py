f94fafb4edb4b39675be74b4cf986720f55b4ee6
HM <hmillionsalt@gmail.com>
2023-07-25 18:45:08 +0900

    config_file

---
+++ pages/07_link.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Link')
+st.write('✓[Yahoo!JAPAN](https://www.yahoo.co.jp/)')
+st.write('✓[DeepL](https://www.deepl.com/ja/translator)')
--- pages/link.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title('Link')
-st.write('[Yahoo!JAPAN](https://www.yahoo.co.jp/)')

249f32e74ac015c30d510b56982e287bf3e8927a
HM <hmillionsalt@gmail.com>
2023-11-12 13:49:52 +0900

    url change

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ if traffic:
         st.write(statusJ.text)
     else:
         st.write('***遅延あり***')
-    '''
-    ###### ▶[JR運行情報]('https://traininfo.jr-central.co.jp/zairaisen/status_detail.html?line=10001&lang=ja')
-    '''
+    
+    st.write('▶[JR運行情報](https://traininfo.jr-central.co.jp/zairaisen/status_detail.html?line=10001&lang=ja)')
+    
     st.markdown('https://traininfo.jr-central.co.jp/zairaisen/status_detail.html?line=10001&lang=ja', unsafe_allow_html=True)
     st.write('')
     '''
